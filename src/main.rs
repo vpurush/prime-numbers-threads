4a044118600c9702df265089b309e74b7932636b
Vijay Purush <vpurush@192-168-1-107.tpgi.com.au>
2023-06-05 22:00:18 +1000

    reducing thread count and using mutexes to collate result

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     thread::{self, JoinHandle},
-    time::Instant,
+    time::Instant, sync::{Mutex, Arc},
 };
 
 fn is_prime(n: u32) -> bool {
@@ -16,21 +16,49 @@ fn is_prime(n: u32) -> bool {
 fn main() {
     let now = Instant::now();
     let mut join_handles: Vec<JoinHandle<()>> = vec![];
-    for i in 1..10000 {
+    let prime_numbers_arc = Arc::new(Mutex::new(vec![]));
+    for i in 1..100000 {
+        let prime_numbers_mutex = Arc::clone(&prime_numbers_arc);
         let handle = thread::spawn(move || {
             if is_prime(i) {
-                println!("{} is prime", i);
+                let mut prime_numbers = prime_numbers_mutex.lock().unwrap();
+                prime_numbers.push(i);
+                drop(prime_numbers);
+                // println!("{} is prime", i);
             } else {
-                println!("{} is not prime", i)
+                // println!("{} is not prime", i);
             }
         });
         join_handles.push(handle);
+
+        if i % 10000 == 0 {
+            join_handles.into_iter().for_each(|join_handle| {
+                join_handle.join().unwrap();
+            });
+
+            join_handles = vec![];
+        }
     }
 
-    join_handles.into_iter().for_each(|join_handle| {
-        join_handle.join().unwrap();
-    });
+    print!("Prime numbers are {:?}", prime_numbers_arc);
 
     let elapsed = now.elapsed();
     println!("Time to execute {:?}", elapsed);
 }
+
+// fn main() {
+//     let now = Instant::now();
+//     let mut prime_numbers = vec![];
+//     for i in 1..100000 {
+//             if is_prime(i) {
+//                 prime_numbers.push(i);
+//                 // println!("{} is prime", i);
+//             } else {
+//                 // println!("{} is not prime", i)
+//             }
+//     }
+//     print!("Prime numbers are {:?}", prime_numbers);
+
+//     let elapsed = now.elapsed();
+//     println!("Time to execute {:?}", elapsed);
+// }
